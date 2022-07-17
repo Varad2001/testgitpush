d28f836e32a66687ed575ff25b77602c599a5658
Varad Khonde <varadkhonde@gmail.com>
2022-07-17 19:30:15 +0530

    Mongodb details

---
+++ test1.py
@@ -0,0 +1,5 @@
+# sudhanshu sir mongodb client
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
